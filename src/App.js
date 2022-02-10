786f2556291db3d8483749aa298a44b1254aa128
Milena Monova <mimonova13@gmail.com>
2022-02-10 21:06:25 +0200

    integrated skill wallet web component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { InitSwAuth } from '@skill-wallet/auth';
+
 
 function App() {
   return (
     <div>
+      <sw-auth partner-key="14a064bf84f70ddb06a560be5351298b3bff4a0c"></sw-auth>
     </div>
   );
 }
 
+InitSwAuth();
 export default App;
